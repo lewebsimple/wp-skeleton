df4ae2e4ceb5b1239ff437e70ad315bbb494e063
Pascal Martineau <pascal@lewebsimple.ca>
2024-12-17 10:04:21 -0500

    wip: acfe flexible content

--- wp-content/themes/skeleton/includes/vendors/acf-extended.php
+++ wp-content/themes/skeleton/includes/vendors/acf-extended.php
@@ -8,3 +8,9 @@ function skeleton_acfe_init() {
 	acf_update_setting( 'acfe/modules/categories', false );
 	acf_update_setting( 'acfe/modules/options_pages', false );
 }
+
+// Determine flexible content render template
+add_filter( 'acfe/flexible/render/template', 'skeleton_acfe_flexible_render_template', 10, 4 );
+function skeleton_acfe_flexible_render_template( $file, $field, $layout, $is_preview ) {
+	return $file;
+}
---
+++ wp-content/themes/skeleton/templates/content/main-page.php
@@ -0,0 +1,3 @@
+<div class="main-page container mx-auto">
+	<?php has_flexible( 'sections' ) && the_flexible( 'sections' ); ?>
+</div>
